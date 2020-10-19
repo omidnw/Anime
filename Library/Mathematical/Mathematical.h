e7a5297819f3b360e77453bedccc112a82d3f005
Omid Reza Keshtkar <omidnw@gmail.com>
2020-10-20 02:13:18 +0330

    ["Fix Problem!", 2:12 AM]

--- Library/Mathematical/Mathematical.h
+++ Library/Mathematical/Mathematical.h
@@ -1,25 +1,37 @@
-#ifndef AnimeMathematical
 #include <stdio.h>
 
-double Aram(double base, double exponent) // Aram == Pow |Aram is a character in Chain Chronicle Anime. | wiki link : https://en.wikipedia.org/wiki/Chain_Chronicle#Characters
+#ifndef AnimeMathematical
+
+long double Aram(long double Base, long double Exponent) /* Aram == Pow |Aram is a character in Chain Chronicle Anime. | wiki link : https://en.wikipedia.org/wiki/Chain_Chronicle#Characters*/
 {
-int result=1;
-if(base == 0 && exponent == 0)
-{
-    printf("\nWTF?\n");
-}
-else if(exponent == 0)
-{
-    return 1;
-}
-else
-{
-    for (exponent; exponent>0; exponent--)
-    {
-        result = result * base;
-    }
-    return result;
-}
+	long double Result=1;
+	if(Base == 0 && Exponent <= 0)
+	{
+		printf("Please Use long double and for print Use %LF");
+	}
+	else if(Exponent == 0)
+	{
+		return Result;
+	}
+	else if(Exponent == 1)
+	{
+		return Base;
+	}
+	else if(Exponent <= 0)
+	{
+		for (long double count = Exponent; count>0; count--)
+		{
+			Result = Result * Base;
+		}
+	}
+	else if(Base >= 0 && Exponent >= 0)
+	{
+		for (long double count = Exponent; count>0; count--)
+		{
+			Result = Result * Base;
+		}
+	}
+	return Result;
 }
 #define AnimeMathematical
 #endif
