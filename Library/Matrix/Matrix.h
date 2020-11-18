5095b516bc6140203dd1c94bbed45ebf199ead92
Aria Khoshnood <khoshnoodaria@gmail.com>
2020-11-18 20:32:26 +0330

    ["Added Matrix lib",5:00PM]

---
+++ Library/Matrix/Matrix.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <assert.h>
+
+float **createArray(int row, int col);
+
+float **multiply(float **_first, float **_second);
+
+float **add(float **_first, float **_second);
